27f789239a9a2d74432831e5552c09f316549732
Cauê Jannini <cauejannini@gmail.com>
2016-10-28 19:33:30 -0200

    ViewPager substituted for normal LinearLayout

---
+++ app/src/main/java/jannini/android/ciclosp/NetworkRequests/GetRouteInterface.java
@@ -0,0 +1,29 @@
+package jannini.android.ciclosp.NetworkRequests;
+
+/**
+ * Created by cauejannini on 04/07/16.
+ */
+public class GetRouteInterface {
+
+    public GetRouteInterface() {
+    }
+
+    public void onResponse (int responseCode, String response) {
+        if (responseCode == 200) {
+            onSuccess(responseCode, response);
+        } else {
+            onFailure(responseCode, response);
+        }
+    }
+
+    public void onSuccess (int responseCode, String response) {
+
+    }
+
+    public void onFailure (int responseCode, String response) {
+
+    }
+
+}
+
+
